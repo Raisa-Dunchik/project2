09a685c8dc0a1f89fb72bc535e18489ac61d1aa9
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-20 22:43:20 +0300

    Employee

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//public class Main {
-  //  public static void main(String[] args) {
-//Employee employee1 = new Employee("Дунчик Раиса Николаевна", "Тестировщик", "145raisa.ro9@gmail.com", "+375298288507", 50000, 25);
-//  employee1.displayInfo();
-// }
-//}
-// Пример использования класса
-        public class Main {
-            public static void main(String[] args) {
-                // Объявляем массив объектов
-                Person[] persArray = new Person[5];
-                // Инициализируем каждую ячейку массива объектами
-                persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "3435466", 30000, 30);
-                persArray[1] = new Person("Petrov Petr", "Manager", "petpetr@mailbox.com", "664524242", 50000, 35);
-                persArray[2] = new Person("Sidorov Sidor", "Developer", "sidisid@mailbox.com", "4235364643", 40000, 28);
-                persArray[3] = new Person("Kuznetsov Kuzya", "Designer", "kuzkuz@mailbox.com", "353466334", 35000, 32);
-                persArray[4] = new Person("Smirnov Smir", "Analyst", "smismir@mailbox.com", "35265356", 45000, 29);
 
-                // Отображаем информацию о всех сотрудниках
-                for (Person person : persArray) {
-                    person.displayInfo();
-                }
-            }
+        // Тестирование класса Park
+        public class Main {
+        public static void main(String[] args) {
+            Park amusementPark = new Park();
+
+            // Добавляем аттракционы
+            amusementPark.addRide("Ferris Wheel", "10:00 - 22:00", 5.0);
+            amusementPark.addRide("Roller Coaster", "10:00 - 20:00", 7.0);
+            amusementPark.addRide("Merry-Go-Round", "10:00 - 21:00", 3.0);
+
+            // Отображаем информацию о всех аттракционах
+            amusementPark.displayRides();
         }
+}
