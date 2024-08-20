dafa9ca575eca1f11943fa56fe1c526505ce2a30
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-20 20:12:03 +0300

    Employee

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Дунчик Раиса Николаевна", "Тестировщик", "145raisa.ro9@gmail.com", "+375298288507", 50000, 25);
+        employee1.displayInfo();
+    }
+}
