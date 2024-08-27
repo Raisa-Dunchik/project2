972c17075303fc4f86af5ed5350f7fa89f0a3ece
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-27 23:48:17 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,54 @@
 
+        public class Main {
+            public static void main(String[] args) {
+                String[][] validArray = {
+                        {"1", "2", "3", "4"},
+                        {"5", "6", "7", "8"},
+                        {"9", "10", "11", "12"},
+                        {"13", "14", "15", "16"}
+                };
 
-public class Main {
-    public static void main(String[] args) {
-        Shape circle = new Circle(5, "Красный", "Черный");
-        Shape rectangle = new Rectangle(4, 6, "Синий", "Зеленый");
-        Shape triangle = new Triangle(3, 4, 5, "Желтый", "Фиолетовый");
+                String[][] invalidArray = {
+                        {"1", "2", "3", "4"},
+                        {"5", "six", "7", "8"}, // здесь ошибка
+                        {"9", "10", "11", "12"},
+                        {"13", "14", "15", "16"}
+                };
 
-        // Вывод информации о фигурах
-        System.out.println("Круг:");
-        System.out.println("Периметр: " + circle.getPerimeter());
-        System.out.println("Площадь: " + circle.getArea());
-        System.out.println("Цвет заливки: " + circle.getColor());
-        System.out.println("Цвет границы: " + circle.getBorderColor());
+                // Вызываем метод суммирования и обработки массива
+                try {
+                    int sum = processArray(validArray);
+                    System.out.println("Сумма элементов массива: " + sum);
+                } catch (MyArraySizeException | MyArrayDataException e) {
+                    System.err.println(e.getMessage());
+                }
 
-        System.out.println("\nПрямоугольник:");
-        System.out.println("Периметр: " + rectangle.getPerimeter());
-        System.out.println("Площадь: " + rectangle.getArea());
-        System.out.println("Цвет заливки: " + rectangle.getColor());
-        System.out.println("Цвет границы: " + rectangle.getBorderColor());
+                // Пример с массивом с ошибками в данных
+                try {
+                    int sum = processArray(invalidArray);
+                    System.out.println("Сумма элементов массива: " + sum);
+                } catch (MyArraySizeException | MyArrayDataException e) {
+                    System.err.println(e.getMessage()); // здесь будет ошибка
+                }
+            }
 
-        System.out.println("\nТреугольник:");
-        System.out.println("Периметр: " + triangle.getPerimeter());
-        System.out.println("Площадь: " + triangle.getArea());
-        System.out.println("Цвет заливки: " + triangle.getColor());
-        System.out.println("Цвет границы: " + triangle.getBorderColor());
-    }
-}
+            public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+                if (array.length != 4 || array[0].length != 4) {
+                    throw new MyArraySizeException("Размер массива должен быть 4х4");
+                }
+
+                int sum = 0;
+
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        try {
+                            sum += Integer.parseInt(array[i][j]);
+                        } catch (NumberFormatException e) {
+                            throw new MyArrayDataException("Ошибка в данных массива на ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                        }
+                    }
+                }
+
+                return sum;
+            }
+        }
